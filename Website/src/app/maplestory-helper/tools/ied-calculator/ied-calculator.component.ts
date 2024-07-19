31af58f141a1952fd562aed786a2e44848295132
Matthew Hwang <matthewhwang95@gmail.com>
2024-07-19 08:53:04 -0400

    Added start of IED calculator

--- Website/src/app/maplestory-helper/tools/ied-calculator/ied-calculator.component.ts
+++ Website/src/app/maplestory-helper/tools/ied-calculator/ied-calculator.component.ts
@@ -1,6 +1,7 @@
 import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Link } from './link';
 
 @Component({
   selector: 'app-ied-calculator',
@@ -11,10 +12,56 @@ import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } f
 })
 export class IedCalculatorComponent implements OnInit {
 
+  links: Link[] = [
+    {name: 'Empirical Knowledge', percentage: 9, visual: false, url: 'Explorer Mage'},
+    {name: 'Light Wash', percentage: 20, visual: true, url: 'Luminous'},
+    {name: "Rhinne's Blessing", percentage: 10, visual: true, url: 'Zero'},
+    {name: 'Bravado', percentage: 10, visual: true, url: 'Hoyoung'},
+  ];
+
+  iedForm: FormGroup = this.formBuilder.group({
+    links: this.formBuilder.array<Link>([])
+  });
+
+  visualIED: number = 0;
+  effectiveIED: number = 0;
+
   constructor(private readonly formBuilder: FormBuilder) {}
+
+  get linksArray(): FormArray<FormGroup> {
+    return (this.iedForm.get('links') as FormArray<FormGroup>);
+  }
   
   ngOnInit() {
+    let linksArray = this.iedForm.get('links') as FormArray;
+    for (let link of this.links) {
+      let row: FormGroup = this.formBuilder.group({
+        name: this.formBuilder.control<string>(link.name),
+        percentage: this.formBuilder.control<number>(link.percentage),
+        visual: this.formBuilder.control<boolean>(link.visual),
+        url: this.formBuilder.control<string>(link.url),
+        include: this.formBuilder.control<boolean>(false)
+      });
+      linksArray.push(row);
+    }
+    this.iedForm.valueChanges.subscribe(() => {
+      this.updateIED();
+    })
   }
 
+  updateIED(): void {
+    let visual = 0;
+    let total = 0;
+    for (let link of this.linksArray.controls) {
+      if (link.get('include').value) {
+        total = (100 - total) * link.get('percentage').value/100 + total;
+        if (link.get('visual').value) {
+          visual = (100 - visual) * link.get('percentage').value/100 + visual;
+        }
+      }
+    }
+    this.visualIED = visual;
+    this.effectiveIED = total;
+  }
 
 }
---
+++ Website/src/app/maplestory-helper/tools/ied-calculator/link.ts
@@ -0,0 +1,6 @@
+export interface Link {
+    name: string
+    percentage: number
+    visual: boolean
+    url: string
+}
